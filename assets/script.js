d975124d36d9824db5605190c24494b3070ca479
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-12-01 09:12:48 -0400

    use set city to execute current condition get request from weather api

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,16 @@ var searchHistoryList = [];
 var lat = cityWeatherResponse.coord.lat;
 var lon = cityWeatherResponse.coord.lon;
 var uviApiURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+//function to get live weather conditions;
+function liveWeather(city) {
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    //use set "city" to execute current condition get request from weather api//
+    $.ajax({
+        url: apiURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        
+    });
+}
